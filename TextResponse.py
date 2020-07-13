0c20e7418f427c9d0c82f24635a639bfed491e38
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-07-13 16:49:14 +1200

    memedate 3

--- TextResponse.py
+++ TextResponse.py
@@ -21,14 +21,13 @@ RANDOM_TALK = [
 DIRECT_RESPONSE = [
     ("img","https://pics.me.me/tim-apple-president-orange-meet-timapple-44217842.png"),
     ("img","https://i.kym-cdn.com/photos/images/newsfeed/001/468/344/fd8.png"),
-    ("img","https://cdn.discordapp.com/emojis/585550292012105729.png?v=1"),
     ("img", "https://i.redd.it/8hkekbti9a221.jpg"),
     "hello, would you like to enroll in a pyramid scheme?",
     "Welcome to seven-eleven",
     "What a great day",
     "Are you bored?\nwell im here to sell you the un-bored-er 5000!!!🔥🔥🔥",
     "anyways . . .\nhttps://en.wikipedia.org/wiki/Integral",
-    "\\🤠/ 🌵\n ▮  🐪\n /\\"
+    "\\\\🤠/ 🌵\n ▮  🐪\n /\\"
 ]
 POWER_urls = [
     "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F032%2F676%2FUnlimited_Power_Banner.jpg",
