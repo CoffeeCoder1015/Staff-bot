a3c9214a815a1bf04eaca5ec151dba0ba383d7e0
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-07-13 18:48:41 +1200

    master

--- Staff.py
+++ Staff.py
@@ -80,13 +80,16 @@ async def on_message(message):
 async def clear(ctx, lmt_arg: int):
     role = str(ctx.author.roles)
 
+    # check if user has x role (so that they won't clear anything only authorized can)
     h_count = 0
     for i in ADMIN_ROLES:
         if i in role:
             h_count += 1
 
+    # has x role
     if h_count != 0:
         await ctx.channel.purge(limit=int(lmt_arg)+1)
+    # doesn't have x role
     else:
         m = random.choice(ERROR_RESPONSE_1)
         if m[0] == "img":
@@ -106,9 +109,39 @@ async def google_meme(ctx):
 
 @client.command()
 async def MATH(ctx, math_arg: str):
+    role = str(ctx.author.roles)
+
+    # check if x user has role (so that they won't list giant numbers,only authorized can)
+    h_count = 0
+    for i in ADMIN_ROLES:
+        if i in role:
+            h_count += 1
+
     try:
         eq = math_arg.replace("^", "**")
-        await ctx.send(str(eval(eq)))
+        v = str(eval(eq))
+        # ---content not bigger than 2000---
+        if len(v) <= 2000:
+            await ctx.send(v)
+        # ---has x role to use the jank char limit overide-er to see the answer---
+        elif h_count != 0 and len(v) <= 30000:  # the limit is there so that it wont bomb the channel with text
+            ts = len(v)//2000
+            v = list(v)
+            nlst = []
+            for _ in range(ts):
+                apo = "".join(v[0:2000])
+                del v[0:2000]
+                nlst.append(apo)
+            if len(v) != 0:
+                apo = "".join(v[0:len(v)])
+                del v[0:len(v)]
+                nlst.append(apo)
+            for i in nlst:
+                await ctx.send(i)
+        # ---too big---
+        else:
+            await ctx.send("TO BIG TO SEND")
+    # ---error handler---
     except:
         m = random.choice(ERROR_RESPONSE_0)
         if m[0] == "img":
@@ -132,6 +165,6 @@ async def clear_err(ctx, error):
         else:
             await ctx.send(m)
 
-with open("TOKEN.txt", "r")as fio:
+with open("Staff-bot/TOKEN.txt", "r")as fio:
     TOKEN = fio.read()
 client.run(TOKEN)
